bb5db43289ea1e25aefe2c969fbb4023c072ac2b
剑平天下1234 <jptx1234@163.com>
2016-10-28 00:12:58 +0800

    Update jpss_run.sh

--- jpss_run.sh
+++ jpss_run.sh
@@ -3,8 +3,8 @@
 rpm -Uvh http://www.city-fan.org/ftp/contrib/yum-repo/rhel7/x86_64/city-fan.org-release-1-13.rhel7.noarch.rpm >/dev/null
 yum install -y httpd curl nss jq bind-utils >/dev/null
 wget -O /var/www/html/index.html https://raw.githubusercontent.com/jptx1234/paste/master/jpss_index.html
-wget -O /var/www/cgi-bin/info.cgi https://raw.githubusercontent.com/jptx1234/paste/master/info.cgi
-chmod 777 /var/www/cgi-bin/info.cgi
+wget -O /var/www/cgi-bin/info.sh https://raw.githubusercontent.com/jptx1234/paste/master/info.sh
+chmod 777 /var/www/cgi-bin/info.sh
 wget -O /etc/httpd/conf/httpd.conf https://raw.githubusercontent.com/jptx1234/paste/master/httpd.conf
 httpd
 sleep 365d
